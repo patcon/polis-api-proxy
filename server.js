4716f267174132cc096511358335d63de780da1e
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-12-03 16:21:33 -0500

    Oops. Zero out Referer on request, not response.

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ argo()
   .use(function(handle) {
     handle('response', function(env, next) {
       env.response.setHeader('Access-Control-Allow-Origin', '*');
-      env.response.setHeader('Referer', '');
+      next(env);
+    });
+    handle('request', function(env, next) {
+      env.request.headers['Referer'] = '';
       console.log(env.request.headers);
       next(env);
     });
     handle('error', function(env, error, next) {
       console.log(error.message);
+      next(env);
+      process.exit();
     });
   })
   .target('https://api.pol.is')
