aa86f6a668b808baf5b9a97f069b56596ea89613
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-12-03 16:02:57 -0500

    Zeroed the Referer header.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ argo()
   .use(function(handle) {
     handle('response', function(env, next) {
       env.response.setHeader('Access-Control-Allow-Origin', '*');
+      env.response.setHeader('Referer', '');
       next(env);
     });
     handle('error', function(env, error, next) {
