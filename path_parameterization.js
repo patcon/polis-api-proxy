8ef038009c711a8b760789bc5538798d3072195a
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-12-31 15:49:43 -0400

    Restricted where we do convo_id rewrite.

--- path_parameterization.js
+++ path_parameterization.js
@@ -6,11 +6,13 @@ module.exports = function(handle) {
     var urlObj = url.parse(env.request.url);
     var pathname = urlObj.pathname;
     var urlParts = pathname.split('/');
-    var conversation_id = urlParts.pop();
-    var obj = qs.stringify({conversation_id: conversation_id});
-    urlObj.search = obj;
-    urlObj.pathname = urlParts.join('/');
-    env.argo.currentUrl = urlObj.format();
+    if (urlParts[3] == 'conversations' && urlParts.length > 4) {
+      var conversation_id = urlParts.pop();
+      var obj = qs.stringify({conversation_id: conversation_id});
+      urlObj.search = obj;
+      urlObj.pathname = urlParts.join('/');
+      env.argo.currentUrl = urlObj.format();
+    };
     next(env);
   });
 };
