e0dfb94f95d9d0801e5c59ca3c8bc07f036a3785
Patrick Connolly <patrick.c.connolly@gmail.com>
2017-01-04 04:52:55 -0400

    Add logic for parsing and rebuilding request body.

--- path_parameterization.js
+++ path_parameterization.js
@@ -12,10 +12,28 @@ module.exports = function(handle) {
         // remove 'conversations' part from path
         urlParts.splice(3, 1);
       };
-      qsOrig = qs.parse(urlObj.query);
+      console.log(env.request.method);
       qsNew = { conversation_id: conversation_id };
-      var queryString = qs.stringify(Object.assign(qsOrig, qsNew));
-      urlObj.search = queryString;
+      switch (env.request.method) {
+        case 'GET':
+          qsOrig = qs.parse(urlObj.query);
+          var queryString = qs.stringify(Object.assign(qsOrig, qsNew));
+          urlObj.search = queryString;
+          break;
+        case 'POST':
+          var body = [];
+          env.request.on('data', function(chunk) {
+            body.push(chunk);
+          }).on('end', function() {
+            body = Buffer.concat(body).toString();
+            var data = JSON.parse(body);
+            data = Object.assign(data, qsNew);
+            data = JSON.stringify(data);
+            env.request.body = data;
+            console.log(data);
+          }).pause();
+          break;
+      };
       urlObj.pathname = urlParts.join('/');
       env.argo.currentUrl = urlObj.format();
       console.log(env.argo.currentUrl);
