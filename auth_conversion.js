324335186b593c538b561ce5a18b84773daac125
Patrick Connolly <patrick.c.connolly@gmail.com>
2023-11-19 17:47:28 +0000

    Only add auth header when apiKey is present.

--- auth_conversion.js
+++ auth_conversion.js
@@ -1,10 +1,12 @@
 module.exports = function(handle) {
   handle('request', function(env, next) {
     var apiKey = env.request.headers['authorization'];
-    var username = apiKey;
-    var password = '';
-    var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
-    env.request.headers['authorization'] = auth;
+    if (apiKey) {
+      var username = apiKey;
+      var password = '';
+      var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
+      env.request.headers['authorization'] = auth;
+    }
 
     // Fake a user-agent to get around cloudflare blocking.
     // TODO: Extract into own export.
